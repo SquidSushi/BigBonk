f52b8050bd0db02c9f62565d6e7b3c5b7121fe87
LinusKolmet <l.kolmet@gmail.com>
2026-05-12 21:01:41 +0200

    Falling animation

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -5,15 +5,19 @@ public class PlayerAnimation : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private float blendSpeed = 5f;
 
+    private PlayerState _playerState;
     private PlayerInputReader input;
 
     private static readonly int inputXHash = Animator.StringToHash("InputX");
-
+    private static int isGroundedHash = Animator.StringToHash("IsGrounded");
+    private static int isFallingHash = Animator.StringToHash("IsFalling");
+    
     private float currentBlend;
 
     private void Awake()
     {
         input = GetComponent<PlayerInputReader>();
+        _playerState = GetComponent<PlayerState>();
     }
 
     private void Update()
@@ -23,6 +27,16 @@ public class PlayerAnimation : MonoBehaviour
 
     private void UpdateAnimationState()
     {
+        bool isIdling = _playerState.CurrentPlayerMovementState == PlayerMovementState.Idling;
+        bool isRunning = _playerState.CurrentPlayerMovementState == PlayerMovementState.Running;
+        bool isSprinting = _playerState.CurrentPlayerMovementState == PlayerMovementState.Sprinting;
+        bool isFalling = _playerState.CurrentPlayerMovementState == PlayerMovementState.Falling;
+        bool isGrounded = _playerState.InGroundedState();
+
+        animator.SetBool(isGroundedHash, isGrounded);
+        animator.SetBool(isFallingHash, isFalling);
+       
+        
         float targetBlend = 0f;
 
         if (input.MovementInput.sqrMagnitude > 0.01f)
